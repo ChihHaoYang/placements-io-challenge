d667544f14d528c5a2e2824514a44d9d80572a8d
Chih-Hao Yang <how30013@gmail.com>
2026-01-10 03:20:46 +0800

    chore: setup eslint + prettier, and lint + format all files

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -13,11 +16,22 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+      reactRefresh.configs.vite
     ],
+    plugins: {
+      prettier: prettierPlugin,
+      'simple-import-sort': simpleImportSort
+    },
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
+    rules: {
+      ...reactHooks.configs.flat.recommended.rules,
+      'prettier/prettier': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    }
   },
+  eslintConfigPrettier
 ])
